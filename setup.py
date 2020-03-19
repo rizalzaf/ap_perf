1035e110d4ac7c4fcd586886c986f1e4083bedf9
Rizal Fathony <rizalaf@live.com>
2020-03-19 14:45:56 -0400

    typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='ap_perf',
       version='0.2.0',
       description='AP-Perf: Incorporating Generic Performance Metrics in Differentiable Learning',
-      url='http://github.com/rizalzaf/ap-perf',
+      url='http://github.com/rizalzaf/ap_perf',
       author='Rizal Fathony',
       author_email='rfathony@cs.cmu.edu',
       license='MIT',
