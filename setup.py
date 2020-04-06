720fafceaf512fc7c93d13048c030f3e2183d2a3
Rizal Fathony <rizalaf@live.com>
2020-04-06 12:28:06 -0400

    change meta

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='ap_perf',
-      version='0.2.0',
+      version='0.1.0',
       description='AP-Perf: Incorporating Generic Performance Metrics in Differentiable Learning',
       url='http://github.com/rizalzaf/ap_perf',
       author='Rizal Fathony',
-      author_email='rfathony@cs.cmu.edu',
+      author_email='rizal@fathony.com',
       license='MIT',
       packages=['ap_perf'],
       install_requires=[
